8efe67c28c2456d7ee2a3d6f5fb570504e969df0
Kulinich Denis <kulinichdenis@ukr.net>
2016-06-13 16:22:36 +0300

    add delete books

--- app/routes/books/index.js
+++ app/routes/books/index.js
@@ -20,19 +20,19 @@ export default Ember.Route.extend({
 		if (!params.page) {
 			return this.store.query('book', { orderBy: 'createdAt', limitToFirst: this.limitToShow});
 		}
-		
-		if(this.currentPage > parseInt(params.page)) { 
+
+		if(this.currentPage > parseInt(params.page)) {
 			this.currentPage = parseInt(params.page);
-			this.spread = this.directionRight ? true : false ;	
-			this.directionRight = false;		
-			
+			this.spread = this.directionRight ? true : false ;
+			this.directionRight = false;
+
 			return this.store.query('book', { orderBy: 'createdAt', endAt: this.prevBook, limitToFirst: this.limitToShow});
-			
-		} else { 			
-			this.currentPage = parseInt(params.page); 
-			this.spread = !this.directionRight ? true : false;	
+
+		} else {
+			this.currentPage = parseInt(params.page);
+			this.spread = !this.directionRight ? true : false;
 			this.directionRight = true;
-			
+
 			return this.store.query('book', { orderBy: 'createdAt', startAt: this.nextBook, limitToFirst: this.limitToShow});
 		}
 	},
@@ -40,15 +40,15 @@ export default Ember.Route.extend({
 	setupController(controller, model) {
 		if (!Ember.isEmpty(model)){
 			if (this.directionRight) {
-				if (this.limitToShow === model.toArray().length) {  
+				if (this.limitToShow === model.toArray().length) {
 						this.prevBook =  this.spread ? this.stepBook : this.currentBook;
 						this.nextBook = model.popObject().get('createdAt');
 						this.currentBook = model.get('lastObject').get('createdAt');
-						this.stepBook = model.get('firstObject').get('createdAt');			
+						this.stepBook = model.get('firstObject').get('createdAt');
 						this.currentPage === 1 ? this._disableButtons(true, false) : this._disableButtons(false, false);
 				} else {
 					this.prevBook = this.spread ? this.stepBook : this.currentBook;
-					this.stepBook = model.get('firstObject').get('createdAt');		
+					this.stepBook = model.get('firstObject').get('createdAt');
 					this._disableButtons(false, true);
 				}
 			} else  {
@@ -63,17 +63,26 @@ export default Ember.Route.extend({
 					this.stepBook = model.get('lastObject').get('createdAt');
 					this._disableButtons(true, false);
 				}
-			} 
+			}
 		}
-		
+
 		this._super(controller, model);
-		
+
 		controller.set('prevButton',  this.prevPageDisable);
 		controller.set('nextButton', this.nextPageDisable);
 		controller.set('nextPage', this.currentPage + 1);
 		controller.set('prevPage', this.currentPage - 1);
 	},
 
+	actions: {
+		deleteBook(book){
+			book.get('library').then((library) => {
+				library.get('books').removeObject(book);
+				library.save();
+			})
+			book.destroyRecord();
+		}
+	},
 	//private methods
 
 	_disableButtons(statPrev, statNext) {
